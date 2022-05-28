42d45fb23a49563d6e0320fedd8aaafd9809ad7c
Lucas Hen <oisouhen@gmail.com>
2022-05-28 12:52:37 -0300

    Vehicles

---
+++ server/resources/[addons]/creativeVehs/fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version "bodacious"
+game "gta5"
+
+client_script "names.lua"
+
+files {
+	"data/*",
+	"audio/*",
+	"audio/**/*"
+}
+
+data_file "CARCOLS_FILE" "data/carcols.meta"
+data_file "HANDLING_FILE" "data/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
+
+data_file "AUDIO_WAVEPACK" "audio/elegyx/dlc_elegyx"
+data_file "AUDIO_GAMEDATA" "audio/elegyx/elegyx_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/elegyx/elegyx_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/r34sound/dlc_r34sound"
+data_file "AUDIO_GAMEDATA" "audio/r34sound/r34sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r34sound/r34sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/hemisound/dlc_hemisound"
+data_file "AUDIO_GAMEDATA" "audio/hemisound/hemisound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/hemisound/hemisound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/argento/dlc_argento"
+data_file "AUDIO_GAMEDATA" "audio/argento/argento_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/argento/argento_sounds.dat"
